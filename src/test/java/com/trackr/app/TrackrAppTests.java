1fb308ff4c99d1971bcd0bc33ebd17efe36ad95c
Timothy Flucker <timflucker@gmail.com>
2022-05-16 14:41:34 -0400

    WIP:Initial project (#6)
    * Add README.md file
    
    * tim flucker adding his introduction
    
    * added some formatting for the README.md
    
    * adding swagger document as project artifact
    
    * updating basepath, and changing paths to use hyphens
    
    * adding initial project files
    
    Co-authored-by: Jean Dorancy <jdorancy@bu.edu>

---
+++ src/test/java/com/trackr/app/TrackrAppTests.java
@@ -0,0 +1,13 @@
+package com.trackr.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackrAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
