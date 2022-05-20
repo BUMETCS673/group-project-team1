8f2f91c89b024d0bfb85bdc45f134d0bd9336172
Tim Flucker <tflucker@gwmail.gwu.edu>
2022-05-20 17:19:10 -0400

    updating unit tests

--- src/test/java/edu/bu/metcs673/trackr/service/impl/UserServiceImplTest.java
+++ src/test/java/edu/bu/metcs673/trackr/service/impl/UserServiceImplTest.java
@@ -2,11 +2,7 @@ package edu.bu.metcs673.trackr.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.stream.Stream;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -19,24 +15,10 @@ public class UserServiceImplTest {
 	@Autowired
 	private UserServiceImpl serviceImpl;
 
-	@ParameterizedTest
-	@MethodSource("generateTestUsers")
-	public void testUserInputValidations(User testUser) {
-
+	@Test
+	public void testDuplicateUsername() {
+		User testUser = new User(0L, "testy", "mcTesterson", "tflucker", "myCoolPassword", "testEmail@email.com");
 		assertThrows(TrackrInputValidationException.class, () -> serviceImpl.validateParameters(testUser));
-	}
-
-	/**
-	 * Creates different objects to be used in the parameterized test. Used to test
-	 * the 'validate' method.
-	 * 
-	 * @return
-	 */
-	private static Stream<Arguments> generateTestUsers() {
-		return Stream.of(Arguments.of(User.builder().username(null).password(null).email(null).build()),
-				Arguments.of(User.builder().username("").password("").email("").build()),
-				Arguments.of(User.builder().username("tflucker").password("blah").email("testEmail@email.com").build()),
-				Arguments.of(User.builder().username("tflucker").password("blah").email(")#(*$)@(##@()*").build()));
 	}
 
 }
