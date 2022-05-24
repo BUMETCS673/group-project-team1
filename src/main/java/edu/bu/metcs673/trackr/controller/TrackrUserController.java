990bdc3d383e297ed3e388bf683df34353e546ab
Tim Flucker <tflucker@gwmail.gwu.edu>
2022-05-24 09:55:00 -0400

    adding new DTO object for the 'retrieve-token' api, updating controllers

--- src/main/java/edu/bu/metcs673/trackr/controller/TrackrUserController.java
+++ src/main/java/edu/bu/metcs673/trackr/controller/TrackrUserController.java
@@ -15,9 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.bu.metcs673.trackr.api.GenericApiResponse;
+import edu.bu.metcs673.trackr.api.TokenRetrievalDTO;
 import edu.bu.metcs673.trackr.api.TrackrUserDTO;
 import edu.bu.metcs673.trackr.common.CommonConstants;
-import edu.bu.metcs673.trackr.domain.TrackrUser;
 import edu.bu.metcs673.trackr.security.JWTUtil;
 import edu.bu.metcs673.trackr.service.TrackrUserService;
 import lombok.extern.slf4j.Slf4j;
@@ -69,7 +69,7 @@ public class TrackrUserController {
 	 * @return
 	 */
 	@PostMapping("/retrieveToken")
-	public ResponseEntity<GenericApiResponse> retrieveToken(@Valid @RequestBody TrackrUser userLogin) {
+	public ResponseEntity<GenericApiResponse> retrieveToken(@Valid @RequestBody TokenRetrievalDTO userLogin) {
 
 		try {
 			UsernamePasswordAuthenticationToken authInputToken = new UsernamePasswordAuthenticationToken(
