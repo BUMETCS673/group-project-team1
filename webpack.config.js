b75d78d83f0469c243733d8180cde685980ba6c7
jdorancy-bu <105374562+jdorancy-bu@users.noreply.github.com>
2022-05-24 15:48:26 -0400

    Frontend Setup (#15)
    * Adding initial dependencies
    
    * Ignore node modules
    
    * Updating dependencies
    
    * Added documentation
    
    * Added simple test cases to ensure index template name is correct

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/main/js/app.js',
+    devtool: 'inline-nosources-cheap-source-map',
+    mode: 'development',
+    output: {
+        path: __dirname,
+        filename: './src/main/resources/static/built/bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }]
+            }
+        ]
+    }
+};
