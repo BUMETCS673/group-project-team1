9679d7c39cb75dc390526c2b182d886e31729ec7
Tim Flucker <tflucker@gwmail.gwu.edu>
2022-06-06 14:12:39 -0400

    updated generic api response classes to consolidate, added new constructors for factory methods with no object provided

--- src/main/java/edu/bu/metcs673/trackr/api/GenericApiResponse.java
+++ src/main/java/edu/bu/metcs673/trackr/api/GenericApiResponse.java
@@ -1,27 +1,27 @@
 package edu.bu.metcs673.trackr.api;
 
-import java.time.LocalDate;
-
-import javax.validation.constraints.NotNull;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-
 import edu.bu.metcs673.trackr.common.CommonConstants;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
 /**
  * Simple object used to contain success and error responses to return
- * meaningful information back to the user.
- * 
- * @author Tim Flucker
+ * meaningful information back to the user. # The purpose of Changing
+ * GenericApiResponse to GenericApiResponse1 is to be convenient for # retrieve
+ * data from the response data without losing data types #
  *
+ * @author Xiaobing Hou
+ * @date 05/25/2022
  */
 @Data
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class GenericApiResponse {
+public class GenericApiResponse<T> {
 
 	@NotNull
 	private String status;
@@ -31,27 +31,53 @@ public class GenericApiResponse {
 	private String message;
 
 	@JsonInclude(Include.NON_EMPTY)
-	private Object additionalData;
+	private T additionalData;
 
 	/**
 	 * Factory method to create a successful response with the provided message.
-	 * Optional object parameter to include returned objects
-	 * 
+	 * Generic object parameter to include returned objects if applicable
+	 *
 	 * @param message
 	 * @return
 	 */
-	public static GenericApiResponse successResponse(String message, Object... obj) {
-		return new GenericApiResponse(CommonConstants.SUCCESS, LocalDate.now(), message, obj);
+	public static <T> GenericApiResponse<T> successResponse(String message, T t) {
+		return new GenericApiResponse<T>(CommonConstants.SUCCESS, LocalDate.now(), message, t);
 	}
 
 	/**
-	 * Factory method to create an error response with the provided message.
-	 * Optional object parameter to include returned objects
-	 * 
+	 * Factory method to create a successful response with the provided message.
+	 *
 	 * @param message
 	 * @return
 	 */
-	public static GenericApiResponse errorResponse(String message, Object... obj) {
-		return new GenericApiResponse(CommonConstants.ERROR, LocalDate.now(), message, obj);
+	public static <T> GenericApiResponse<T> successResponse(String message) {
+		return new GenericApiResponse<T>(CommonConstants.SUCCESS, LocalDate.now(), message);
+	}
+
+	/**
+	 * Factory method to create an error response with the provided message. Generic
+	 * object parameter to include returned objects if applicable.
+	 *
+	 * @param message
+	 * @return
+	 */
+	public static <T> GenericApiResponse<T> errorResponse(String message, T t) {
+		return new GenericApiResponse<>(CommonConstants.ERROR, LocalDate.now(), message, t);
+	}
+
+	/**
+	 * Factory method to create a error response with the provided message.
+	 *
+	 * @param message
+	 * @return
+	 */
+	public static <T> GenericApiResponse<T> errorResponse(String message) {
+		return new GenericApiResponse<T>(CommonConstants.ERROR, LocalDate.now(), message);
+	}
+
+	public GenericApiResponse(String status, LocalDate date, String message) {
+		this.status = status;
+		this.date = date;
+		this.message = message;
 	}
 }
