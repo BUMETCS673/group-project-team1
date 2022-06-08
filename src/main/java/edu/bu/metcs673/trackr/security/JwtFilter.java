f5a2ce5df6507e11ed496d49c4f0fc890d2bdfac
Tim Flucker <tflucker@gwmail.gwu.edu>
2022-06-08 14:02:14 -0400

    updating security related classes

--- src/main/java/edu/bu/metcs673/trackr/security/JwtFilter.java
+++ src/main/java/edu/bu/metcs673/trackr/security/JwtFilter.java
@@ -1,6 +1,8 @@
 package edu.bu.metcs673.trackr.security;
 
 import com.auth0.jwt.exceptions.JWTVerificationException;
+
+import edu.bu.metcs673.trackr.common.CommonConstants;
 import edu.bu.metcs673.trackr.user.TrackrUserServiceImpl;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +31,10 @@ import static edu.bu.metcs673.trackr.common.CommonConstants.JWT_COOKIE_NAME;
 import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 
 /**
- * Filter class that runs before actual API logic is processed. Checks for a JWT token and tries to
- * verify it. Successfully verification will pass the request to its respective controller, while
- * unsuccessful verification will return an unauthorized response to the user.
+ * Filter class that runs before actual API logic is processed. Checks for a JWT
+ * token and tries to verify it. Successfully verification will pass the request
+ * to its respective controller, while unsuccessful verification will return an
+ * unauthorized response to the user.
  * <p>
  * Reference:
  * https://medium.com/geekculture/implementing-json-web-token-jwt-authentication-using-spring-security-detailed-walkthrough-1ac480a8d970
@@ -41,134 +44,119 @@ import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
  */
 @Component
 public class JwtFilter extends OncePerRequestFilter {
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String BEARER_PREFIX = "Bearer ";
-    public static final String FAVICON_PATH = "/favicon.ico";
-    public static final String IMAGES_PATH = "/images/.*";
-    public static final String GENERATED_ASSETS_PATH = "/built/.*";
-    public static final String ROOT_PATH = "/";
-    public static final String REGISTER_PATH = "/register";
-    public static final String LOGIN_PATH = "/login";
-    public static final String LOGOUT_PATH = "/logout";
-    private final TrackrUserServiceImpl userServiceImpl;
-    private final JWTUtil jwtUtil;
 
-    /**
-     * JWT Filter constructor
-     *
-     * @param userServiceImpl User service
-     * @param jwtUtil         JWT Util
-     */
-    public JwtFilter(@Autowired TrackrUserServiceImpl userServiceImpl, @Autowired JWTUtil jwtUtil) {
-        this.userServiceImpl = userServiceImpl;
-        this.jwtUtil = jwtUtil;
-    }
+	@Autowired
+	private TrackrUserServiceImpl userServiceImpl;
 
-    /**
-     * Filter method that checks for the JWT authorization, if it finds a JWT then verification otherwise unauthorized.
-     * Note: There are certain paths that should skip the filter see {@link #shouldNotFilter(HttpServletRequest)}.
-     *
-     * @param request  Http Servlet Request
-     * @param response Http Servlet Response
-     * @param chain    Filter chain which has to be called in order apply subsequent filters
-     * @throws IOException      Checked exception
-     * @throws ServletException Checked exception
-     */
-    @Override
-    protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain chain) throws IOException, ServletException {
+	@Autowired
+	private JWTUtil jwtUtil;
 
-        // Fetch the token
-        String jwt = getJwt(request);
 
-        // If the  token was present either in header or cookie but, it was a blank string.
-        if (StringUtils.isBlank(jwt)) {
-            response.sendError(SC_UNAUTHORIZED, "Invalid JSON Web Token (JWT).");
-            return;
-        }
+	public JwtFilter(@Autowired TrackrUserServiceImpl service, @Autowired JWTUtil util) {
+		this.userServiceImpl = service;
+		this.jwtUtil = util;
+	}
 
-        try {
+	/**
+	 * Filter method that checks for the JWT authorization, if it finds a JWT then
+	 * verification otherwise unauthorized. Note: There are certain paths that
+	 * should skip the filter see {@link #shouldNotFilter(HttpServletRequest)}.
+	 *
+	 * @param request  Http Servlet Request
+	 * @param response Http Servlet Response
+	 * @param chain    Filter chain which has to be called in order apply subsequent
+	 *                 filters
+	 * @throws IOException      Checked exception
+	 * @throws ServletException Checked exception
+	 */
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
 
-            // Validate token and return the username contained within
-            String username = jwtUtil.validateTokenAndRetrieveSubject(jwt);
-            if (StringUtils.isNotBlank(username)) {
-                UserDetails userDetails = userServiceImpl.loadUserByUsername(username);
+		// Fetch the token
+		String jwt = getJwt(request);
 
-                // Create token using the username and password gathered from the JWT token
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        username,
-                        userDetails.getPassword(),
-                        new ArrayList<>()
-                );
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+		// If the token was present either in header or cookie but, it was a blank
+		// string.
+		if (StringUtils.isBlank(jwt)) {
+			response.sendError(SC_UNAUTHORIZED, "Invalid JSON Web Token (JWT).");
+			return;
+		}
 
-                // Set authentication for API request
-                if (SecurityContextHolder.getContext().getAuthentication() == null) {
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
-                }
-            }
-        } catch (JWTVerificationException | UsernameNotFoundException e) {
-            response.sendError(SC_UNAUTHORIZED, "Invalid JWT Token");
-            return;
-        }
+		try {
 
-        // Continue with the filter chain
-        chain.doFilter(request, response);
-    }
+			// Validate token and return the username contained within
+			String username = jwtUtil.validateTokenAndRetrieveSubject(jwt);
+			if (StringUtils.isNotBlank(username)) {
+				UserDetails userDetails = userServiceImpl.loadUserByUsername(username);
 
-    /**
-     * Checks for the paths the filter should skip. Avoid giving permission to range of pages.
-     * <a href="https://www.baeldung.com/spring-exclude-filter">Should Not Filter Tutorial</a>
-     *
-     * @param request Servlet request
-     * @return boolean
-     * @throws ServletException Checked exception
-     */
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        List<Pattern> patterns = Arrays.asList(
-                Pattern.compile(FAVICON_PATH), // Images from static assets
-                Pattern.compile(IMAGES_PATH), // Images from static assets
-                Pattern.compile(GENERATED_ASSETS_PATH), // Generate assets from Webpack build
-                Pattern.compile(ROOT_PATH), // Index page
-                Pattern.compile(REGISTER_PATH), // Registration page
-                Pattern.compile(LOGIN_PATH), // login page
-                Pattern.compile(LOGOUT_PATH) // Logout page
-        );
+				// Create token using the username and password gathered from the JWT token
+				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username,
+						userDetails.getPassword(), new ArrayList<>());
+				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-        // If we match one then we should not filter
-        for (Pattern pattern : patterns) {
-            if (pattern.matcher(request.getRequestURI()).matches()) {
-                return true;
-            }
-        }
+				// Set authentication for API request
+				if (SecurityContextHolder.getContext().getAuthentication() == null) {
+					SecurityContextHolder.getContext().setAuthentication(authToken);
+				}
+			}
+		} catch (JWTVerificationException | UsernameNotFoundException e) {
+			response.sendError(SC_UNAUTHORIZED, "Invalid JWT Token");
+			return;
+		}
 
-        return false;
-    }
+		// Continue with the filter chain
+		chain.doFilter(request, response);
+	}
 
-    /**
-     * Get the JWT from the request. It can come from a cookie or Http Authorization header.
-     *
-     * @param request Http request
-     * @return String
-     */
-    private String getJwt(HttpServletRequest request) {
-        String authHeader = Optional.ofNullable(request.getHeader(AUTHORIZATION_HEADER)).orElse("");
-        String jwt = "";
+	/**
+	 * Checks for the paths the filter should skip. Avoid giving permission to range
+	 * of pages. <a href="https://www.baeldung.com/spring-exclude-filter">Should Not
+	 * Filter Tutorial</a>
+	 *
+	 * @param request Servlet request
+	 * @return boolean
+	 * @throws ServletException Checked exception
+	 */
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+		List<Pattern> patterns = Arrays.asList(Pattern.compile(CommonConstants.FAVICON_PATH), // Images from static assets
+				Pattern.compile(CommonConstants.IMAGES_PATH), // Images from static assets
+				Pattern.compile(CommonConstants.GENERATED_ASSETS_PATH), // Generate assets from Webpack build
+				Pattern.compile(CommonConstants.ROOT_PATH), // Index page
+				Pattern.compile(CommonConstants.REGISTER_PATH), // Registration page
+				Pattern.compile(CommonConstants.LOGIN_PATH), // login page
+				Pattern.compile(CommonConstants.LOGOUT_PATH) // Logout page
+		);
 
-        // If the request has an Authorization header which is a Bearer token (JWT) then attempt to verify it
-        if (authHeader.startsWith(BEARER_PREFIX)) {
-            return authHeader.substring(7);
-        }
+		// If we match one then we should not filter
+		return patterns.stream().anyMatch(pattern -> pattern.matcher(request.getRequestURI()).matches());
 
-        // If no header then check the cookie
-        if (StringUtils.isBlank(jwt)) {
-            Cookie cookie = WebUtils.getCookie(request, JWT_COOKIE_NAME);
-            jwt = Optional.ofNullable(cookie).map(Cookie::getValue).orElse("");
-        }
+	}
 
-        return jwt;
-    }
+	/**
+	 * Get the JWT from the request. It can come from a cookie or Http Authorization
+	 * header.
+	 *
+	 * @param request Http request
+	 * @return String
+	 */
+	private String getJwt(HttpServletRequest request) {
+		String authHeader = Optional.ofNullable(request.getHeader(CommonConstants.AUTHORIZATION_HEADER)).orElse("");
+		String jwt = "";
+
+		// If the request has an Authorization header which is a Bearer token (JWT) then
+		// attempt to verify it
+		if (authHeader.startsWith(CommonConstants.BEARER_PREFIX)) {
+			return authHeader.substring(7);
+		}
+
+		// If no header then check the cookie
+		if (StringUtils.isBlank(jwt)) {
+			Cookie cookie = WebUtils.getCookie(request, JWT_COOKIE_NAME);
+			jwt = Optional.ofNullable(cookie).map(Cookie::getValue).orElse("");
+		}
+
+		return jwt;
+	}
 }
