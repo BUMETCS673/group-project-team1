9fa68f39d9a486088ed1b9fa591bc0bbc6a7f882
Tim Flucker <tflucker@gwmail.gwu.edu>
2022-06-14 13:55:25 -0400

    updating web security config

--- src/main/java/edu/bu/metcs673/trackr/security/WebSecurityConfig.java
+++ src/main/java/edu/bu/metcs673/trackr/security/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import edu.bu.metcs673.trackr.user.TrackrUserServiceImpl;
 
@@ -55,7 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable())
+		http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
 			.httpBasic()
 			.disable()
 			.cors()
